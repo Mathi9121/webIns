37005a177deecc10edb953b2fbcd69e783d7bceb
roundge <etienne.runge@gmail.com>
2014-09-04 10:09:05 +0200

    intégration du bundle wkthmltopdf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new OCIM\UserBundle\OCIMUserBundle(),
             new OCIM\ExportBundle\OCIMExportBundle(),
             new OCIM\ContactsBundle\OCIMContactsBundle(),
+			new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
