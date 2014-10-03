030f6c62994fddd5607fba766c5c5a58ea6cde4c
roundge <etienne.runge@gmail.com>
2014-10-03 17:47:27 +0200

    add des fichiers (crud) de lajout de lentite Template

---
+++ src/OCIM/ExportBundle/Form/TemplateType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OCIM\ExportBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TemplateType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('type')
+            ->add('date')
+            ->add('contenu')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OCIM\ExportBundle\Entity\Template'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ocim_exportbundle_template';
+    }
+}
