e53ea6d06430c096e048716428674369ab630421
roundge <etienne.runge@gmail.com>
2014-10-31 18:13:35 +0100

    templating, de bonnes idees pour un beau dashboard

--- app/Resources/Public/js/main.js
+++ app/Resources/Public/js/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-	setSidebarSize();
-	window.onresize = setSidebarSize;
 	
 	$('#user a').click(function(){
 		$(this).next('ul').animate({ height: "toggle"}, 300);
 	});
 	
+	$("#nav-toggle").click(function(){
+	var state = parseInt($('#content').css('margin-left')) > 200;
+	console.log(state);
+		$("#sidebar").animate({
+			"left": (state ? -215 : 0)
+		});
+		$("#content").animate({
+			"margin-left": (state ? 0 : 215)
+		});
+	});
+	
 });
-
-function setSidebarSize(){
-	$("#sidebar").css('height', $(document).innerHeight());
-}
