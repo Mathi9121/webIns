decb52895c7e55c19878e024e3b576e6371a3d72
Mathi9121 <mathilde.boucard@outlook.fr>
2018-04-19 10:24:37 +0200

    ;=>,

--- src/OCIM/EvenementsBundle/Controller/ChampPersoController.php
+++ src/OCIM/EvenementsBundle/Controller/ChampPersoController.php
@@ -139,7 +139,7 @@ class ChampPersoController extends Controller
         $form = $this->createForm(ChampPersoType::class, $entity, array(
             'action' => $this->generateUrl('champPerso_create'),
             'method' => 'POST',
-            'trait_types' => $traitTypes;
+            'trait_types' => $traitTypes,
         ));
 
         $form->add('submit', SubmitType::class, array('label' => 'Créer'));
