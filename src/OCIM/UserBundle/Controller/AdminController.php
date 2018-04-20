964fa1da025a8ed6166285e149876be5fa775a4c
Mathi9121 <mathilde.boucard@outlook.fr>
2018-04-20 10:11:15 +0200

    SecurityContext => Securite

--- src/OCIM/UserBundle/Controller/AdminController.php
+++ src/OCIM/UserBundle/Controller/AdminController.php
@@ -3,7 +3,7 @@
 namespace OCIM\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
@@ -14,16 +14,16 @@ class AdminController extends Controller
 		$session = $request->getSession();
 
 		//On regarde les erreurs
-		if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)){
-			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+		if($request->attributes->has(Security::AUTHENTICATION_ERROR)){
+			$error = $request->attributes->get(Security::AUTHENTICATION_ERROR);
 		}
 		else{
-			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+			$error = $session->get(Security::AUTHENTICATION_ERROR);
+			$session->remove(Security::AUTHENTICATION_ERROR);
 		}
 		return $this->render("OCIMUserBundle:Admin:connexion.html.twig", 
 			array(
-			'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+			'last_username' => $session->get(Security::LAST_USERNAME),
 			'error'=> $error
 			));
     }
