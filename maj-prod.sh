62a21229c6f9b77d31358ee7589bc458984ebde0
Mathi9121 <mathilde.boucard@outlook.fr>
2018-04-25 14:30:16 +0200

    ajout de services dans config.yml

--- maj-prod.sh
+++ maj-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bin/console cache:clear --env=prod
-chown -R www-data:www-data  bin/cache/ bin/logs/
+chown -R www-data:www-data  var/cache/ var/logs/
 bin/console assets:install --env=prod
 bin/console assetic:dump --env=prod
