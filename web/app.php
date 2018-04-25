4cabed5f22903f221dda2fcd4a7b54e3bd3fa695
Mathi9121 <mathilde.boucard@outlook.fr>
2018-04-25 12:02:29 +0200

    ajout de Request::HEADER_X_FORWARDED_ALL

--- web/app.php
+++ web/app.php
@@ -22,7 +22,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('prod', false);
 //$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-Request::setTrustedProxies(['~']);
+Request::setTrustedProxies(['~'], Request::HEADER_X_FORWARDED_ALL);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
