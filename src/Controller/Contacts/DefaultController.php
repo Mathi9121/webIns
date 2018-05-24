c4e89792ed8f9370614a974f20b39e3b7742a0f0
Mathi9121 <mathilde.boucard@outlook.fr>
2018-05-24 16:01:45 +0200

    Supp test -- 4.0

--- src/Controller/Contacts/DefaultController.php
+++ src/Controller/Contacts/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function index($name)
-    {//test
+    {
         return $this->render('Contacts/Default/index.html.twig', array('name' => $name));
     }
 }
